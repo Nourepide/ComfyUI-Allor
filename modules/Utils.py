5e08298fcda349cef8db284a695b0a25055164a6
Nourepide <nourepide@gmail.com>
2023-06-08 05:34:04 +0300

    Add samplers to ImageDraw module

--- modules/Utils.py
+++ modules/Utils.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import torchvision.transforms as t
 from PIL import Image as ImageF
 from PIL.Image import Image as ImageB
@@ -14,6 +16,23 @@ def image_to_tensor(self):
 
 def create_rgba_image(width: int, height: int, color=(0, 0, 0, 0)) -> ImageB:
     return ImageF.new("RGBA", (width, height), color)
+
+
+def get_sampler_by_name(method) -> Literal[0, 1, 2, 3, 4, 5]:
+    if method == "lanczos":
+        return ImageF.LANCZOS
+    elif method == "bicubic":
+        return ImageF.BICUBIC
+    elif method == "hamming":
+        return ImageF.HAMMING
+    elif method == "bilinear":
+        return ImageF.BILINEAR
+    elif method == "box":
+        return ImageF.BOX
+    elif method == "nearest":
+        return ImageF.NEAREST
+    else:
+        raise ValueError("Sampler not found.")
 
 
 Tensor.tensor_to_image = tensor_to_image
