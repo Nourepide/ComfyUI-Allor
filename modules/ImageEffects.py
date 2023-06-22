030ef8c78a78f6c5dab90eae79ffb1b0c362177c
Nourepide <nourepide@gmail.com>
2023-06-22 21:27:03 +0300

    Add ImageEffectsLensZoomBurst node.

--- modules/ImageEffects.py
+++ modules/ImageEffects.py
@@ -188,6 +188,148 @@ class ImageEffectsSepia:
         return (tensor,)
 
 
+class ImageEffectsLensZoomBurst:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "scale": ("FLOAT", {
+                    "default": 1.5,
+                    "min": 1.0,
+                    "step": 0.01
+                }),
+
+                "samples": ("INT", {
+                    "default": 100,
+                    "min": 1,
+                }),
+                "position_x": ("FLOAT", {
+                    "default": 0.5,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "position_y": ("FLOAT", {
+                    "default": 0.5,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "rotation": ("FLOAT", {
+                    "default": 0.0,
+                    "min": 0.0,
+                    "max": 360.0,
+                }),
+                "method": (["circle", "point"],),
+                "stabilization": (["true", "false"],),
+
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "node"
+    CATEGORY = "image/effects/lens"
+
+    # noinspection PyUnresolvedReferences
+    def zoom_burst(
+            self,
+            image,
+            scale,
+            samples,
+            position,
+            rotation,
+            method,
+            stabilization,
+    ):
+        if scale < 1.0:
+            raise ValueError("Parameter scale can't be smaller then initial image size.")
+
+        h, w = image.shape[:2]
+
+        x = np.arange(w)
+        y = np.arange(h)
+
+        xx, yy = np.meshgrid(x, y)
+
+        cx = int(w * position[0])
+        cy = int(h * position[1])
+
+        if method == "circle":
+            d = np.sqrt((xx - cx) ** 2 + (yy - cy) ** 2)
+            max_d = np.sqrt((w / 2) ** 2 + (h / 2) ** 2)
+
+            map_x_up = (xx - d * (scale - 1.0) / max_d * (xx - cx) / samples).astype(np.float32)
+            map_y_up = (yy - d * (scale - 1.0) / max_d * (yy - cy) / samples).astype(np.float32)
+
+            map_x_down = (xx + d * (scale - 1.0) / max_d * (xx - cx) / samples).astype(np.float32)
+            map_y_down = (yy + d * (scale - 1.0) / max_d * (yy - cy) / samples).astype(np.float32)
+        elif method == "point":
+            map_x_up = (xx - (xx - cx) * (scale - 1.0) / samples).astype(np.float32)
+            map_y_up = (yy - (yy - cy) * (scale - 1.0) / samples).astype(np.float32)
+
+            map_x_down = (xx + (xx - cx) * (scale - 1.0) / samples).astype(np.float32)
+            map_y_down = (yy + (yy - cy) * (scale - 1.0) / samples).astype(np.float32)
+        else:
+            raise ValueError("Unsupported method.")
+
+        if rotation > 0.0:
+            angle_step = rotation / samples
+
+            rm_up = cv2.getRotationMatrix2D((cx, cy), angle_step, 1)
+            rm_down = cv2.getRotationMatrix2D((cx, cy), -angle_step, 1)
+        else:
+            vibration_angle = 1.0
+            vibration_step = vibration_angle / samples
+
+            rm_up_even = cv2.getRotationMatrix2D((cx, cy), vibration_step, 1)
+            rm_down_even = cv2.getRotationMatrix2D((cx, cy), -vibration_step, 1)
+
+            rm_up_odd = cv2.getRotationMatrix2D((cx, cy), -vibration_step, 1)
+            rm_down_odd = cv2.getRotationMatrix2D((cx, cy), vibration_step, 1)
+
+        for i in range(samples):
+            if stabilization:
+                tmp_up = cv2.remap(image, map_x_up, map_y_up, cv2.INTER_LINEAR)
+                tmp_down = cv2.remap(image, map_x_down, map_y_down, cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
+
+                if rotation > 0.0:
+                    tmp_up = cv2.warpAffine(tmp_up, rm_up, (w, h), borderMode=cv2.BORDER_REFLECT)
+                    tmp_down = cv2.warpAffine(tmp_down, rm_down, (w, h), borderMode=cv2.BORDER_REFLECT)
+                else:
+                    if i % 2 == 0:
+                        tmp_up = cv2.warpAffine(tmp_up, rm_up_even, (w, h), borderMode=cv2.BORDER_REFLECT)
+                        tmp_down = cv2.warpAffine(tmp_down, rm_down_even, (w, h), borderMode=cv2.BORDER_REFLECT)
+                    else:
+                        tmp_up = cv2.warpAffine(tmp_up, rm_up_odd, (w, h), borderMode=cv2.BORDER_REFLECT)
+                        tmp_down = cv2.warpAffine(tmp_down, rm_down_odd, (w, h), borderMode=cv2.BORDER_REFLECT)
+
+                image = cv2.addWeighted(tmp_up, 0.5, tmp_down, 0.5, 0)
+            else:
+                tmp = cv2.remap(image, map_x_up, map_y_up, cv2.INTER_LINEAR)
+
+                if rotation > 0.0:
+                    rm = cv2.getRotationMatrix2D((cx, cy), angle_step, 1)
+                    tmp = cv2.warpAffine(tmp, rm, (w, h), borderMode=cv2.BORDER_REFLECT)
+                else:
+                    if i % 2 == 0:
+                        tmp = cv2.warpAffine(tmp, rm_up_even, (w, h), borderMode=cv2.BORDER_REFLECT)
+                    else:
+                        tmp = cv2.warpAffine(tmp, rm_up_odd, (w, h), borderMode=cv2.BORDER_REFLECT)
+
+                image = cv2.addWeighted(tmp, 0.5, image, 0.5, 0)
+
+        return image
+
+    def node(self, images, scale, samples, position_x, position_y, rotation, method, stabilization):
+        tensor = images.clone().detach()
+
+        return (cv2_layer(tensor, lambda x: self.zoom_burst(
+            x, scale, samples, (position_x, position_y), rotation, method, True if stabilization == "true" else False
+        )),)
+
+
 class ImageEffectsLensChromaticAberration:
     def __init__(self):
         pass
@@ -442,8 +584,8 @@ class ImageEffectsLensBokeh:
             raise ValueError("Unsupported blur type.")
 
         return (cv2_layer(tensor, lambda x: self.lens_blur(
-            x, blades_shape, blades_radius, blades_rotation, method)
-        ),)
+            x, blades_shape, blades_radius, blades_rotation, method
+        )),)
 
 
 class ImageEffectsLensOpticAxis:
@@ -612,6 +754,7 @@ NODE_CLASS_MAPPINGS = {
     "ImageEffectsGrayscale": ImageEffectsGrayscale,
     "ImageEffectsNegative": ImageEffectsNegative,
     "ImageEffectsSepia": ImageEffectsSepia,
+    "ImageEffectsLensZoomBurst": ImageEffectsLensZoomBurst,
     "ImageEffectsLensChromaticAberration": ImageEffectsLensChromaticAberration,
     "ImageEffectsLensBokeh": ImageEffectsLensBokeh,
     "ImageEffectsLensOpticAxis": ImageEffectsLensOpticAxis,
